59f563adcde5df36cae931a9a0854a6b425940f3
rajveer1131 <pal.ritik15@gmail.com>
2023-10-13 16:51:12 +0530

    Remade The Game with simple ui changes for better user experience

--- index.js
+++ index.js
@@ -1,75 +1,117 @@
 let pieces = ["ROCK", "PAPER", "SCISSORS"];
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
+    
     return choice;
 }
 
-function play(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection == pieces[0]) {
-        if (computerSelection == pieces[2]) {
+    if (playerSelection == "ROCK") {
+        if (computerSelection == "SCISSORS") {
+            console.log("Round Winner = Player");
             return 1;
-        } else if (computerSelection == pieces[0]) {
-            return 0;
+        } else if (computerSelection == "ROCK") {
+            console.log("Round Tied");
         } else {
+            console.log("Round Winner = Computer");
             return -1;
         }
     }
 
-    else if (playerSelection == pieces[1]) {
-        if (computerSelection == pieces[0]) {
+    else if (playerSelection == "PAPER") {
+        if (computerSelection == "ROCK") {
+            console.log("Round Winner = Player");
             return 1;
-        } else if (computerSelection == pieces[1]) {
-            return 0;
+        } else if (computerSelection == "PAPER") {
+            console.log("Round Tied");
         } else {
+            console.log("Round Winner = Computer");
             return -1;
         }
     }
 
-    else if (playerSelection == pieces[2]) {
-        if (computerSelection == pieces[1]) {
+    else if (playerSelection == "SCISSORS") {
+        if (computerSelection == "PAPER") {
+            console.log("Round Winner = Player");
             return 1;
-        } else if (computerSelection == pieces[2]) {
-            return 0;
+        } else if (computerSelection == "SCISSORS") {
+            console.log("Round Tied");
         } else {
+            console.log("Round Winner = Computer");
             return -1;
         }
     }
 
 }
 
-function game() {
-    let counter = 0;
-    for (let i = 1; i <= 5; i++) {
-        let playerSelection = prompt(
-            `Enter your Choice: \n("Rock","Paper","Scissors")`
-        ).toUpperCase();
-        let computerSelection = pieces[getComputerChoice()].toUpperCase();
-        console.log("You chose: " + playerSelection);
-        console.log("Computer chose: " + computerSelection);
-        let result = play(playerSelection, computerSelection);
+
+const btn=document.querySelectorAll('button');
+
+const rock=document.querySelector('rock');
+const paper=document.querySelector('paper');
+const scissors=document.querySelector('scissors');
+
+
+
+
+let pCounter=0;
+let cCounter=0;
+
+    btn.forEach((e)=>{
+        e.addEventListener('click',(ex)=>{
+            
+        const pRes=document.querySelector('#playerResult');
+        const cRes=document.querySelector('#computerResult');
+        const finalRes=document.querySelector('#finalResult');
+        
+        let playerSelection = e.value.toUpperCase();
+        let computerSelection = pieces[getComputerChoice()];
+
+        let result = playRound(playerSelection, computerSelection);
         if (result == 1) {
-            console.log("Round "+i+" Winner = Player");
-            counter++;
+            
+            pCounter++;
+            console.log("P:"+pCounter);
+            // updateVal();
+            pRes.textContent=pCounter;
         }
         else if (result == -1) {
-            console.log("Round "+i+" Winner = Computer");
-            counter--;
+            
+            cCounter++;
+            console.log("C: "+cCounter);
+            // updateVal();
+            cRes.textContent=cCounter;
         }
-        else{
-            console.log("Round "+i+" Tied");
-        }
-        // console.log(play(playerSelection, computerSelection));
-    }
-    if (counter > 0) {
-        console.log("\n\nYou won the game, your score is: " + counter);
-    }
-    else if (counter == 0) {
-        console.log("\n\nGame tied\n");
-    }
-    else {
 
-        console.log("\n\nYou lost the game, your score is: " + counter);
-    }
-}
-game();
+        if(pCounter==5){
+            finalRes.textContent="Player Won ";
+           finalResult();
+        }
+        else if(cCounter==5){
+            finalRes.textContent="Computer Won ";
+            finalResult();
+        }
+        function finalResult(){
+            cRes.textContent=0;
+            pRes.textContent=0;
+            cCounter=0;
+            pCounter=0;
+        }
+                
+
+        
+        console.log("You chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
+    })
+})
+
+
+
+
+
+
+
+
+
+
